025157ac323d5e092b9ad38beeab0e25066002a1
Techpc23 <mh2624127@gmail.com>
2021-02-15 17:48:08 +0600

    Google Email Authentication.... GoogleApiClient.Builder  --This method is deprecated but still working.

---
+++ app/src/main/java/com/example/google_authentication/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.google_authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
